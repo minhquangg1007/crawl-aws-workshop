93131c81c8a567980d21bc46526c90a6b650ce8b
quang <quangphammrr19@gmail.com>
2026-07-14 18:28:56 +0700

    feat: upate href element

--- get-url-path.js
+++ get-url-path.js
@@ -4,7 +4,7 @@ const elementName = process.env.ELEMENT_NAME || 'my-class'; // Default class nam
 const elements = document.querySelectorAll(`.${elementName}`);
 
 // 2. Map over the elements to get their text content
-const allText = Array.from(elements).map(element => element.textContent).join(" ");
+const allText = Array.from(elements).map(element => element.href).join(" ");
 
 // 3. Log the resulting array to see all the text content
 console.log(allText);
